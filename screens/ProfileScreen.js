1dae93995f1a0f5f3db11b2790de4f23e04d0897
edkristiand <edkristian.daguit@gmail.com>
2019-02-12 14:46:07 +0800

    Added Profile Screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,6 +6,7 @@ import {
     Image,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import * as Progress from 'react-native-progress';
 
 export default class ProfileScreen extends Component {
     static navigationOptions = {
@@ -16,25 +17,56 @@ export default class ProfileScreen extends Component {
         super(props);
         this.state = {
           profName: 'Richard',
-          profDetails: 'View and edit profile'
+          profDetails: 'View and edit profile',
+          profSteps: 'You need to complete your details before you can apply for a loan, so get a head start by doing it now.',
+          incInfo: 'Incomplete Info',
+          profImg: ''
         }
     }
 
     render() {
+        let profImage;
+
+        if(this.state.profImg) {
+            profImage = <Image 
+                style={{width: 64, height: 64}}
+                source={{uri: this.state.profImg}}
+            />
+        } else {
+            profImage = <Icon
+                name='user-circle'
+                type='font-awesome'
+                color='#e2e2e2'
+                size={80}
+                iconStyle={{marginBottom: 15, flex: 1}}
+            />
+        }
         return (
             <View style={styles.mainContainer}>
                 <View style={styles.nameImageContainer}>
                     <View style={{flex: 3}}>
                         <Text style={styles.name}>{this.state.profName}</Text>
-                        <Text>{this.state.profDetails}</Text>
+                        <Text style={styles.viewProf}>{this.state.profDetails}</Text>
                     </View>
-                    <Icon
-                        name='user-circle'
-                        type='font-awesome'
-                        color='#e2e2e2'
-                        size={80}
-                        iconStyle={{marginBottom: 15, flex: 1}}
+
+                    {profImage}
+
+                </View>
+                <View style={styles.progress}>
+                    <View style={styles.incContainer}>
+                        <Text style={styles.incInfoHeader}>Just 4 steps</Text>
+                        <Text style={styles.incInfo}>{this.state.incInfo}</Text>
+                    </View>
+                    <Progress.Bar 
+                        progress={0.25} 
+                        borderWidth={0} 
+                        borderRadius={0} 
+                        width={null} 
+                        height={10}
+                        color='#429cf4'
+                        unfilledColor='#c6e0fb'
                     />
+                    <Text style={styles.defaultText}>{this.state.profSteps}</Text>
                 </View>
                 <View style={styles.stepsContainer}>
                     <View style={styles.profileMenu}>
@@ -108,14 +140,19 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         paddingTop: 30,
-        paddingRight: 20,
-        paddingLeft: 20
+        paddingRight: 30,
+        paddingLeft: 30
     },
     nameImageContainer: {
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         paddingBottom: 20,
         paddingTop: 20
+    },
+    progress: {
+        flex: 1,
+        marginBottom: 30
     },
     stepsContainer: {
         flex: 3,
@@ -135,6 +172,28 @@ const styles = StyleSheet.create({
         paddingBottom: 15
     },
     profileMenuText: {
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    defaultText: {
+        fontSize: 16,
+        marginTop: 15
+    },
+    incContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingBottom: 15
+    },  
+    incInfoHeader: {
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    incInfo: {
+        color: '#f86e7b',
+        fontSize: 18,
+        fontWeight: '600'
+    },
+    viewProf: {
         fontSize: 18
     }
 })
